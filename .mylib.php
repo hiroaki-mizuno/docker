5ba81ca7f522e0861227612e73725bda651f631d
hiroaki-mizuno <mizuno@diverta.co.jp>
2015-10-27 19:46:00 +0900

    unuse password

--- .mylib.php
+++ .mylib.php
@@ -24,3 +24,28 @@ runkit_function_redefine("setDGF", '$key, $id = 0', 'return "MY_DG_CODE";');
 runkit_function_redefine("getDGF", '$key, $id = 0', 'return "MY_DG_CODE";');
 runkit_function_redefine("setDGB", '$key, $id = 0', 'return "MY_DG_CODE";');
 runkit_function_redefine("getDGB", '$key, $id = 0', 'return "MY_DG_CODE";');
+
+require_once(LIB_PATH."/modules/login/class/Login.php");
+
+class MyRCMSLoginChallenger extends RCMSLoginChallenger {
+    public function challenge() {
+        global $cn;
+
+        // DB読込み(メンバーテーブルから認証)
+        $strSQL =
+                "select a.member_id, a.login_pwd as pass, a.login_pwd_md5 as hashed_pass, a.pass_salt, a.name1, a.name2, a.disp_name, a.email, a.nickname, a.api_key, a.force_chpwd, a.login_ok_ymd " .
+                "from v_member_header a " .
+                "where login_id = 'diverta' ";
+        $result = selectQuery($cn, $strSQL);
+
+        if ($row = getRow($result)) {
+
+            RCMSUser::setLogin($row, $_REQUEST['login_save']);
+
+            LoginHistory::write($cn, RCMSUser::getUser());
+            $_SESSION["password_inputted"] = 1;
+            return array(true, ($_REQUEST['login_save'] == 1));
+        }
+        return array(true, ($_REQUEST['login_save'] == 1));
+    }
+}
